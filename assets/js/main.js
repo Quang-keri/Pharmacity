00d0aeb8d72d1c63890e847904aba1cdfa2d5a56
Quang-keri <phamquang040102@gmail.com>
2025-04-21 03:01:24 +0700

    fix slide btn

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,16 @@ owl.owlCarousel({
 // $(".owl-next").click(function () {
 //   owl.trigger("next.owl.carousel");
 // });
+
+var owl = $(".owl-carousel");
+owl.owlCarousel();
+// Go to the next item
+$(".customNextBtn").click(function () {
+  owl.trigger("next.owl.carousel");
+});
+// Go to the previous item
+$(".customPrevBtn").click(function () {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger("prev.owl.carousel", [300]);
+});
